bbed1a5fc0f97ada29b649aba5b258d82142f3d0
MartinMZen <mmelekus@users.noreply.github.com>
2022-05-24 11:50:15 -0500

    Added EF references and WorldCitiesDB SSDT project

---
+++ WorldCitiesDB/Security/WorldCitiesAdmin_1.sql
@@ -0,0 +1,3 @@
+﻿CREATE LOGIN [WorldCitiesAdmin]
+    WITH PASSWORD = N'{+ca|.lla$ir|gRiPzkKbsdqmsFT7_&#$!~<OshBrgray3n~', SID = 0x636598DCE78C3D4398DF6CEA3DBA2C23, DEFAULT_DATABASE = [WorldCities], DEFAULT_LANGUAGE = [us_english], CHECK_POLICY = OFF;
+
