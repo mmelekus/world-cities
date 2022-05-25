9b7a2b63d42c09e6b3cbc6f222cb22d1cacf473a
MartinMZen <mmelekus@users.noreply.github.com>
2022-05-25 11:01:57 -0500

    Imported models into SSDT that were generated by EF migrations

--- WorldCitiesAPI/Program.cs
+++ WorldCitiesAPI/Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 // Add ApplicationDbContext and SQL Server support
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
---
+++ WorldCitiesDB/dbo/Tables/Countries.sql
@@ -0,0 +1,23 @@
+﻿CREATE TABLE [dbo].[Countries] (
+    [Id]   INT            IDENTITY (1, 1) NOT NULL,
+    [Name] NVARCHAR (450) NOT NULL,
+    [ISO2] NVARCHAR (450) NOT NULL,
+    [ISO3] NVARCHAR (450) NOT NULL,
+    CONSTRAINT [PK_Countries] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Countries_Name]
+    ON [dbo].[Countries]([Name] ASC);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Countries_ISO3]
+    ON [dbo].[Countries]([ISO3] ASC);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Countries_ISO2]
+    ON [dbo].[Countries]([ISO2] ASC);
+
