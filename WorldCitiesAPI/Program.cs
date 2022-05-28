3af95065c971e3782317dee151dbbb32a40b9805
MartinMZen <mmelekus@users.noreply.github.com>
2022-05-28 12:47:00 -0500

    Read CORS AllowedHosts from appsettings file

--- WorldCitiesAPI/Program.cs
+++ WorldCitiesAPI/Program.cs
@@ -3,18 +3,19 @@ using Microsoft.EntityFrameworkCore;
 using WorldCitiesAPI.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
 
 // Add services to the container.
 builder.Services.AddCors(options =>
 {
-    options.AddDefaultPolicy(builder => { builder.WithOrigins("*"); });
+    options.AddDefaultPolicy(builder => { builder.WithOrigins(configuration.GetValue<string>("AllowedHosts")); });
 });
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddSwaggerGen();
 // Add ApplicationDbContext and SQL Server support
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
